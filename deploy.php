9d6f96e74b5e33408e0bb11dcf03b9821bde44f8
Tobias Götz <tobias.goetz1@gmx.net>
2024-06-20 12:39:39 +0200

    test

--- deploy.php
+++ deploy.php
@@ -11,10 +11,11 @@ set('application', 'my_typo3_project');
 set('repository', 'git@github.com:CRONEXi/typo3.git');
 
 // Hosts
-host('157.90.119.127')
-    ->user('CRONEX_STAGING')
-    ->identityFile('~/.ssh/id_rsa_ci_cd')
-    ->set('deploy_path', '/var/www/html/{{application}}');
+host('157.90.119.127') // Replace 'your-hetzner-ip' with the actual IP address of your Hetzner VPS
+    ->set('remote_user', 'CRONEX_STAGING') // Replace 'your-username' with the SSH username you use to log into your Hetzner VPS
+    ->set('identity_file', '~/.ssh/id_rsa_ci_cd') // Path to your SSH private key
+    ->set('deploy_path', '/var/www/html/{{application}}'); // Path where you want to deploy the application on your server
+
 
 // Tasks
 task('deploy', [
